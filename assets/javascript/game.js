87be9d6a0cee4e8485fe2fab629b3067436d9d64
unknown <ilyrodriguez@gmail.com>
2019-03-13 18:33:47 -0400

    still not finished yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,97 @@
+confirm("                           -  WELCOME TO THE PSYCHIC GAME  -    \n" +
+    "                                guess what letter I'm thinking of?\n" +
+    "\n" +
+    "                     You have up to 10 guesses to win or lose a point\n" +
+    "                              Win 10 points and you are a Psychic\n" +
+    "                               Loose 10 points and is game over\n" +
+    "\n" +
+    "                                   WOULD YOU LIKE TO PLAY?");
 
-alert("Welcome to The Physic Game " + "guess what letter I'm thinking, you have up to 10 guesses");
+// Array of letters
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var userGuess = document.getElementById("user-guess");
-// var answer = "I";
-var guessLetters = [];
-// ("what's your guess?");
-var computerChoice = "";
 
+// Variables 
 var wins = 0;
 var losses = 0;
-var guesses = 10;
+var guessesLeft = 9;
+var userGuess = [];
 
-function pickRandom (){
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var randomNumber = Math.floor(Math.random()*letters.length);
+//reset to start the game again
+// function reset() {
+//     guessesLeft = 9;
+//     guesses = [];
+    // computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    // console.log(computerGuess);
+// }
+  
+// var userGuess = document.getElementById("user-guess");
+// var computerChoice = [];
+// ("what's your guess?");
+// var computerChoice = " ";
 
-    var randomLetter = letters[randomNumber];
-    computerChoice = randomLetter;
+// function newGame() {
+//     wins = 0;
+//     losses = 0;
+//     userGuess = 9;
+//     document.getElementById('user-guess').innerHTML = "Your guesses so far:  ";
+//     computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+//     console.log(computerGuess);
+// }
+
+function pickRandom() {
+    userGuess = [];
+    computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    console.log(computerGuess);
+}
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 }
 
-userGuess.innerHTML += " " + guessLetters;
 pickRandom();
+document.getElementById('guessesLeft').innerHTML += " " + guessesLeft;
 
-    document.onkeypress = function(event) {
-        var userGuess = event.key;
-        document.getElementById('user-guess').innerHTML += userGuess;
-    
-        if(userGuess === computerChoice){
-            wins++;
-            attempts = 10;
-            usedArray = [];
-        }else{
-            guesses--;
-        }
-    
-        if(guesses !== computerChoice){
-            losses++;
-        }
-    
-        document.getElementById('wins').innerHTML = "Wins: " + wins;
-        document.getElementById('losses').innerHTML = "losses: " + losses;
-        document.getElementById('guesses').innerHTML = "Guesses left: " + guesses;
-    
-    }   
+document.onkeypress = function (event) {
+    var userGuess = event.key.toLocaleLowerCase();
+    document.getElementById('user-guess').innerHTML += userGuess;
+
+pickRandom()
+    if (userGuess === computerGuess) {
+        wins++;
+        alert(" You win 1 point ");
+        pickRandom();
+    } 
+    else {
+      guessesLeft--;
+    }
+
+    if (guessesLeft === 0) {
+        losses++;
+     }
+
+    if (wins === 10) {
+        alert("                                  -     CONGRATULATIONS    -\n " +
+            "                                      You are a real Psychic! ");
+        console.log(wins);
+        // wins = 0;
+        // losses = 0;
+        // userGuess = 9;
+        // guesses = [];
+    }
+  
+
+    if (losses === 10) {
+        alert("                                         -    GAME OVER   -\n" +
+            "                                         You are no Psychic! ");
+        console.log(losses);
+        wins = 0;
+        losses = 0;
+        userGuess = 9;
+        guesses = [];
+    }
+
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('losses').innerHTML = "losses: " + losses;
+    // document.getElementById('userGuess').innerHTML = "Guesses left: " + userGuess;
+
+}
